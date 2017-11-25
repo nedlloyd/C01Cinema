b2d935d603ecdb55ab2326f15803b3a612ff6592
Samuel Bradshaw <samuel.bradshaw.14@ucl.ac.uk>
2017-11-25 13:07:57 +0000

    Entering a correct user name and password now launches either a new admin or user window. I will now begin working on the admin window

--- JavaCinema/src/application/MainController.java
+++ JavaCinema/src/application/MainController.java
@@ -7,8 +7,12 @@ import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -21,56 +25,109 @@ public class MainController {
 	@FXML
 	private TextField txtPassword;
 
-	/*public static void main(String[] args) throws FileNotFoundException {
+	/**
+	 * Returns a BufferedReader of a file.
+	 * @param filePath A String signifying the location of the file.
+	 * @return a BufferedReader of the file at location filePath.
+	 * @throws FileNotFoundException
+	 */
+	public BufferedReader getBufferedReaderFromFile(String filePath) throws FileNotFoundException{
+		FileReader file = new FileReader (filePath);
+		BufferedReader br = new BufferedReader(file);
+		return br;
+	}
 
-	} */
+	/**
+	 * Method triggered when the user presses the log in Button on the Login window. 
+	 * @param event
+	 * @throws Exception
+	 */
+	public void Login(ActionEvent event) throws Exception {
 
-	public void Login(ActionEvent event) throws FileNotFoundException {
-
+		//Storing the user name and password that the user enters into the login TextFields. 
 		String usernameAttempt = txtUserName.getText();
 		String passwordAttempt = txtPassword.getText();
 
-		FileReader file = new FileReader ("src/loginDetails/login_details.txt");
-		BufferedReader reader = new BufferedReader(file);
+		//Storing all of the valid user names and passwords in a BufferedReader object.  
+		BufferedReader reader = getBufferedReaderFromFile("src/loginDetails/login_details.txt");
+
 		Scanner scanner = new Scanner(reader);
 		boolean userMatched = false;
-		while (scanner.hasNextLine() && userMatched == false) {
 
+		//Scan through all of the user names and passwords until a match is found
+		while (scanner.hasNextLine() && userMatched == false) {
+			
+			//Create a new Scanner object for each line where the data of each user is stored. 
 			String line = scanner.nextLine();
 			Scanner lineScanner = new Scanner(line);
-			System.out.println(line);
+
 			while (lineScanner.hasNext()) {
-				String user = lineScanner.next();
+				
+				String userName = lineScanner.next();
 				String password = lineScanner.next();
-				boolean isAdmin = false;
-				if (usernameAttempt.equals(user)) {
-					if (passwordAttempt.equals(password)) {
-						if (lineScanner.hasNext()) {
-							isAdmin = true;
+
+				if (usernameAttempt.equals(userName)) {			// If the user name and
+					if (passwordAttempt.equals(password)) {		// password entered is a match...  
+						
+						//Check to see if user is an administrator:
+						if (lineScanner.hasNext() && lineScanner.next().equals("admin")) { 
+
 							lblstatus.setText("Login as admin is a success!!!");
-							lineScanner.close();
 							userMatched = true;
+							lineScanner.close();
+
+							// Launch the new admin window:
+							try{
+								Stage adminStage = new Stage();
+								Parent root = FXMLLoader.load(getClass().getResource("/application/AdminMain.fxml"));
+								Scene scene = new Scene(root,400,400);
+								scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+								adminStage.setScene(scene);
+								adminStage.show();
+							}
+							catch(Exception e){
+								System.out.println("Launching Admin Scene failed: "+e);
+							}
+
 							break;
-						} else {
+
+						} else /*If the user is NOT an admin (is a regular user)*/ { 
 							lblstatus.setText("Login as user is a success!!!");
 							lineScanner.close();
 							userMatched = true;
+
+							// Launch the new user window: 
+							try{
+								Stage userStage = new Stage();
+								Parent root = FXMLLoader.load(getClass().getResource("/application/UserMain.fxml"));
+								Scene scene = new Scene(root,400,400);
+								scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+								userStage.setScene(scene);
+								userStage.show();
+							}
+							catch(Exception e){
+								System.out.println("Launching Admin Scene failed: "+e);
+							}
+
 							break;
 						}
-					} else {
+					} else /*If user name entered is valid but password is wrong*/{
 						lblstatus.setText("login failed, incorrect password");
 						lineScanner.close();
 						break;
 					} 
-				} else {
+				} else /*If user name or password entered does not match the current item in the lineScanner*/{
+					lineScanner.close();
 					break;
 				}
 			}
-			if (scanner.hasNextLine() == false && userMatched ==false) {
-				lblstatus.setText("login failed, incorrect username");
-				break;	
+			if (scanner.hasNextLine() == false && userMatched == false) {	// If we have scanned the whole 
+				lblstatus.setText("User name not recognised");		// document of possible users
+				lineScanner.close();										// and a match has not been found	
 			}
 		}
+
+		scanner.close();
 
 
 
