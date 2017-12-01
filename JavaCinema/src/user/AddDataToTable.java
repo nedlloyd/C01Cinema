d0206241889054f9baa00d502f056abee975c8e2
Ned <nedparrylloyd@gmail.com>
2017-12-01 23:54:36 +0000

    added view films

---
+++ JavaCinema/src/user/AddDataToTable.java
@@ -0,0 +1,40 @@
+package user;
+
+import java.time.LocalTime;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class AddDataToTable {
+		private SimpleStringProperty filmName, filmDescription, filmTime;
+
+		
+		public AddDataToTable(String filmName, String filmDescription, String filmTime) {
+		this.filmName = new SimpleStringProperty(filmName);
+		this.filmDescription = new SimpleStringProperty(filmDescription);
+		this.filmTime = new SimpleStringProperty(filmTime);				
+		}
+		
+		public String getFilmDescription() {
+			return filmDescription.get();
+		}
+		
+		public void setFilmDescription(SimpleStringProperty filmDescription) {
+			this.filmDescription = filmDescription;
+		}
+		
+		public String getFilmName() {
+			return filmName.get();
+		}
+		
+		public void setFilmName(SimpleStringProperty filmName) {
+			this.filmName = filmName;
+		}
+		
+		public String getFilmTime() {
+			return filmTime.get();
+		}
+		
+		public void setFilmTime(SimpleStringProperty filmTime) {
+			this.filmTime = filmTime;
+		}
+}
